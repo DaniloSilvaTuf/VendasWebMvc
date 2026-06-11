de59261fdd1965928ad06584b357d5079523c800
Danilo Silva <danilo.silva.rp@gmail.com>
2026-06-11 15:33:58 -0300

    Implementando busca agrupada

--- VendasWebMvc/Controllers/HistoricoVendasController.cs
+++ VendasWebMvc/Controllers/HistoricoVendasController.cs
@@ -35,9 +35,23 @@ namespace VendasWebMvc.Controllers
             return View(resultado);
         }
 
-        public IActionResult BuscaAgrupada()
+        public async Task<IActionResult> BuscaAgrupada(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            if (!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+
+            var resultado = await _historicoVendasService.FindByDateGroupingAsync(minDate, maxDate);
+            return View(resultado);
         }
     }
 }
--- VendasWebMvc/Models/HistoricoVendas.cs
+++ VendasWebMvc/Models/HistoricoVendas.cs
@@ -1,11 +1,16 @@
-﻿using VendasWebMvc.Models.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using VendasWebMvc.Models.Enums;
 
 namespace VendasWebMvc.Models
 {
     public class HistoricoVendas
     {
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0: dd/MM/yyyy}")]
         public DateTime Data { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Valor { get; set; }
         public VendasStatus Status { get; set; }
         public Vendedor Vendedor { get; set; }
